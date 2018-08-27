3d8211d66d92e272981dfe0c5e52737599518438
bylee <bylee78@gmail.com>
2018-08-27 10:16:00 +0900

    Add missing sources because of .gitignore

---
+++ client/apm/src/test/java/hera/build/MonitorServerTest.java
@@ -0,0 +1,25 @@
+/*
+ * @copyright defined in LICENSE.txt
+ */
+
+package hera.build;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+
+public class MonitorServerTest {
+
+  protected final transient Logger logger = getLogger(getClass());
+
+  @Test
+  public void testServer() {
+    final MonitorServer monitorServer = new MonitorServer();
+    monitorServer.setPort(0);
+    monitorServer.boot(true);
+    logger.info("Server boot");
+    monitorServer.down(true);
+    logger.info("Server down");
+  }
+}
---
+++ client/apm/src/test/java/hera/build/web/service/BuildServiceTest.java
@@ -0,0 +1,42 @@
+/*
+ * @copyright defined in LICENSE.txt
+ */
+
+package hera.build.web.service;
+
+import static java.util.UUID.randomUUID;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import hera.build.web.model.BuildDetails;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BuildServiceTest {
+  protected BuildDetails result1 = new BuildDetails();
+  protected BuildDetails result2 = new BuildDetails();
+  protected BuildDetails result3 = new BuildDetails();
+  protected BuildDetails result4 = new BuildDetails();
+  protected BuildDetails result5 = new BuildDetails();
+
+  protected BuildService buildService;
+
+  @Before
+  public void setUp() {
+    buildService = new BuildService();
+    buildService.save(result1);
+    buildService.save(result2);
+    buildService.save(result3);
+    buildService.save(result4);
+    buildService.save(result5);
+  }
+
+  @Test
+  public void testList() {
+    assertEquals(3, buildService.list(result2.getUuid(), 3).size());
+    assertEquals(3, buildService.list(null, 3).size());
+    assertEquals(1, buildService.list(result5.getUuid(), 3).size());
+    assertNull(buildService.list(randomUUID().toString(), 3));
+  }
+
+}
