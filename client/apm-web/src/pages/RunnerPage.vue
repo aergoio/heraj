17e35d15d87221544546606ec2f3b5e83ac5f386
bylee <bylee78@gmail.com>
2018-08-28 17:25:09 +0900

    Add a checker if address is empty

--- client/apm-web/src/pages/RunnerPage.vue
+++ client/apm-web/src/pages/RunnerPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="row">
     <b-dropdown id="target-selector" :text="text" class="m-md-2">
       <b-dropdown-item v-for="item in targets" :key="item.name" @click="itemSelected(item)">{{item.name}}</b-dropdown-item>
     </b-dropdown>
@@ -8,7 +8,7 @@
       <b-dropdown text="Select" variant="info" slot="prepend">
         <b-dropdown-item v-for="item in addresses" :key="item.name" @click="addressSelected(item)">{{item.name}}</b-dropdown-item>
       </b-dropdown>
-      <b-form-input v-model="address"></b-form-input>
+      <b-form-input v-model="address" placeholder="Input your privatekey."></b-form-input>
     </b-input-group>
 
     <b-button @click="loadClicked">Load</b-button>
@@ -34,9 +34,15 @@
         this.$data.address = item.name;
       },
       loadClicked() {
-        const alreadyExists = this.$data.addresses.find(address => address.name == this.$data.address)
+        if (this.$data.address || '' == this.$data.address) {
+          alert('No target address');
+          return ;
+        }
+
+        const address = this.$data.address
+        const alreadyExists = this.$data.addresses.find(address => address.name == address)
         if (!alreadyExists) {
-          this.$data.addresses.push({name: this.$data.address});
+          this.$data.addresses.push({ name: address });
         }
         this.$http.get('/contract/' + address).then((res) => {
           console.log('Response', res);
