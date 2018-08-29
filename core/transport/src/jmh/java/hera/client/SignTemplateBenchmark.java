4771f16cb424f9b1f5e92ef960083d35456d830e
bylee <bylee78@gmail.com>
2018-08-29 17:22:14 +0900

    Remove unused field

--- core/transport/src/jmh/java/hera/client/SignTemplateBenchmark.java
+++ core/transport/src/jmh/java/hera/client/SignTemplateBenchmark.java
@@ -12,8 +12,6 @@ import hera.api.model.Hash;
 import hera.api.model.Transaction;
 import hera.util.pki.ECDSAKey;
 import hera.util.pki.ECDSAKeyGenerator;
-import io.grpc.ManagedChannel;
-import io.grpc.netty.NettyChannelBuilder;
 import java.util.concurrent.atomic.AtomicLong;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -22,7 +20,6 @@ import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
 
 @BenchmarkMode(Mode.Throughput)
 public class SignTemplateBenchmark {
@@ -32,8 +29,6 @@ public class SignTemplateBenchmark {
   @State(Scope.Thread)
   public static class User {
 
-    protected ManagedChannel channel = null;
-
     protected SignTemplate signTemplate;
 
     protected ECDSAKey senderKey;
@@ -42,17 +37,8 @@ public class SignTemplateBenchmark {
 
     @Setup(Level.Trial)
     public synchronized void setUp() throws Exception {
-      channel = NettyChannelBuilder
-          .forAddress("localhost", 7845)
-          .usePlaintext()
-          .build();
       signTemplate = new SignTemplate();
       senderKey = new ECDSAKeyGenerator().create();
-    }
-
-    @TearDown(Level.Trial)
-    public void tearDown() {
-      channel.shutdown();
     }
 
     public void sendTransaction() throws Exception {
