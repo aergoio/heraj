d85ef370880e9b9d0cf23443b204ae4992ea75dd
bylee <bylee78@gmail.com>
2018-09-14 18:12:28 +0900

    Fix mistype

--- client/apm-web/src/main.js
+++ client/apm-web/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Axios from 'Axios'
+import Axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueMoment from 'vue-moment'
 import App from './App'
