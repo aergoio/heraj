4350aabb0e2f3ec77642aa13623be6e5ce8d5c6f
bylee <bylee78@gmail.com>
2018-09-18 15:39:52 +0900

    Remove runner

--- core/common/src/test/java/hera/api/SignTemplateTest.java
+++ core/common/src/test/java/hera/api/SignTemplateTest.java
@@ -7,13 +7,12 @@ package hera.api;
 import static java.util.UUID.randomUUID;
 import static org.junit.Assert.assertTrue;
 
-import hera.AbstractTestCase;
 import hera.AergoKey;
 import hera.AergoKeyGenerator;
 import hera.api.model.BytesValue;
 import org.junit.Test;
 
-public class SignTemplateTest extends AbstractTestCase {
+public class SignTemplateTest {
 
   @Test
   public void test() throws Exception {
