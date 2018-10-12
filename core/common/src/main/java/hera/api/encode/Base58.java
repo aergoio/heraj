2c1f3859ba7ec0d5d69f1d9d409674107fdbebd0
Taeik Lim <sibera21@gmail.com>
2018-10-12 19:21:48 +0900

    Add IOException to base58

--- core/common/src/main/java/hera/api/encode/Base58.java
+++ core/common/src/main/java/hera/api/encode/Base58.java
@@ -6,11 +6,12 @@ package hera.api.encode;
 
 import hera.api.model.BytesValue;
 import hera.util.Base58Utils;
+import java.io.IOException;
 
 public interface Base58 extends EncodedString {
 
   @Override
-  default BytesValue decode() {
+  default BytesValue decode() throws IOException {
     return BytesValue.of(Base58Utils.decode(getEncodedValue()));
   }
 
