4600f88ca0dbce15be375c6e37811e0219684300
Taeik Lim <sibera21@gmail.com>
2018-10-18 17:19:21 +0900

    Remove lock in account

--- core/common/src/main/java/hera/api/model/AbstractAccount.java
+++ core/common/src/main/java/hera/api/model/AbstractAccount.java
@@ -5,59 +5,38 @@
 package hera.api.model;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 
-@ToString(exclude = "mutex")
+@ToString
 @EqualsAndHashCode
 public abstract class AbstractAccount implements Account {
 
-  protected final Object mutex = this;
-
+  @Getter
   protected long nonce = 1;
 
+  @Getter
   protected long balance = 0;
 
   @Override
   public void bindState(final AccountState state) {
-    synchronized (mutex) {
-      setNonce(state.getNonce());
-      setBalance(state.getBalance());
-    }
+    setNonce(state.getNonce());
+    setBalance(state.getBalance());
   }
 
   @Override
   public void setNonce(final long nonce) {
-    synchronized (mutex) {
-      this.nonce = nonce <= 0 ? 1 : nonce;
-    }
-  }
-
-  @Override
-  public long getNonce() {
-    synchronized (mutex) {
-      return this.nonce;
-    }
+    this.nonce = nonce <= 0 ? 1 : nonce;
   }
 
   @Override
   public long getNonceAndImcrement() {
-    synchronized (mutex) {
-      return this.nonce++;
-    }
+    return this.nonce++;
   }
 
   @Override
   public void setBalance(final long balance) {
-    synchronized (mutex) {
-      this.balance = balance < 0 ? 0 : balance;
-    }
-  }
-
-  @Override
-  public long getBalance() {
-    synchronized (mutex) {
-      return this.balance;
-    }
+    this.balance = balance < 0 ? 0 : balance;
   }
 
 }
